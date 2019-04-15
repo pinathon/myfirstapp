ea74b489ec78eae09e58fe722c4543c05ad8e423
vinayskywalker <vinaykumar.dakshana17@gmail.com>
2019-04-16 01:54:29 +0530

    Sign up and Login using Firebase

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -11,30 +11,73 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity{
-
-
-    private EditText editTextEmail;
-    private EditText editTextPassword;
-    private Button buttonSignup;
-    private FirebaseAuth firebaseAuth;
-
+public class MainActivity extends AppCompatActivity {
+    private TextView email;
+    private Button signOut;
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editTextEmail = findViewById(R.id.editTextemail);
-        editTextPassword = findViewById(R.id.editTextpassword);
-        buttonSignup = findViewById(R.id.sign_up);
+        //get firebase auth instance
+        auth = FirebaseAuth.getInstance();
+        //get current user
+       /* FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        firebaseAuth = FirebaseAuth.getInstance();
+        if(user!=null){
+            authListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
 
+                }
+            };
+        }
+        if (user == null) {
+            // user auth state is changed - user is null
+            // launch login activity
+            startActivity(new Intent(MainActivity.this, login.class));
+            finish();
+        }*/
+        signOut = (Button) findViewById(R.id.sign_out);
+        email = (TextView) findViewById(R.id.email);
+        //assert user != null;
+       // email.setText(user.getEmail());
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
     }
+
+
+    //sign out method
+    public void signOut() {
+        auth.signOut();
+    }
+    /*
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
+    }*/
 }
